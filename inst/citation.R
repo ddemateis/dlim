a77419f51bb008f4799a09746ec91383929bb546
Danielle Demateis <Danielle.Demateis@colostate.edu>
2024-04-25 14:26:23 -0600

    pkgdown changes

---
+++ inst/citation.R
@@ -0,0 +1,13 @@
+## R package reference generated from DESCRIPTION metadata
+citation(auto = meta)
+
+## DLIM paper
+bibentry(bibtype = "Article",
+         title = "Penalized distributed lag interaction model: Air pollution, birth weight, and neighborhood vulnerability",
+         author = c(person(c("Danielle"), "Demateis"),
+                    person(c("Kayleigh", "P."), "Keller"),
+                    person(c("David"), "Rojas-Rueda"),
+                    person(c("Marianthi-Anna"), "Kioumourtzoglou"),
+                    person(c("Ander"), "Wilson")),
+         pages = {e2843},
+         year = "2024", journal = "Environmetrics", doi = "10.1002/env.2843")
